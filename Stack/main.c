a82e8f5e55574ec1940e054d5f580d535a2b6232
enes <eng.eneskaya@gmail.com>
2022-06-14 13:12:51 +0300

    Initial Commit

---
+++ Stack/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Stack
+//
+//  Created by Enes Kaya on 14.06.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
